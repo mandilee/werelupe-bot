ea304d6e678193bc82e06807ced3c8d19d982686
kreo tsai <43966453+kreotsai@users.noreply.github.com>
2022-01-27 20:06:36 +0000

    i missed one oops, disabled /pong for real now maybe

--- bot.js
+++ bot.js
@@ -66,9 +66,9 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+ // if (interaction.commandName === 'ping') {
+  //  await interaction.reply('Pong!');
+  //}
 });
 
 //RoDaddy Stuff
