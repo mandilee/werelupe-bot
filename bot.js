19c07a6ef95cc50b8419b14a1b142098ed13056e
Rodolfo <Rodolfitoe@gmail.com>
2022-02-18 04:17:44 +0000

    Added Valentine color and did some fixes to usuki colors

--- bot.js
+++ bot.js
@@ -121,13 +121,31 @@ client.on("messageCreate", (message) => {//Do Not Close This Function Till Later
 
   //Neopet Responder Starts Here - Rodaddy 2021
   //new Regex to determine a neopet is mentioned
-  let neopet = /.*(8-bit|Agueena|Alien|Angel|Apple|Asparagus|Aubergine|Avocado|Baby|Biscuit|Blue|Blueberry|Brown|Burlap|Camouflage|Candy|Carrot|Checkered|Chocolate|Chokato|Christmas|Clay|Cloud|Coconut|Coffee|Corn|Custard|Darigan|Desert|Dimensional|Disco|Durian|Elderlyboy|Elderlygirl|Electric|Eventide|Faerie|Fire|Garlic|Ghost|Glass|Glowing|Gold|Gooseberry|Grape|Green|Grey|Halloween|Ice|Invisible|Island|Jelly|Juppieswirl|Lemon|Lime|Magma|Mallow|Maractite|Maraquan|Marble|Mosaic|Msp|Mutant|Oilpaint|Onion|Orange|Origami|Pastel|Pea|Peach|Pear|Pepper|Pineapple|Pink|Pirate|Plum|Plushie|Polkadot|Purple|Quigukiboy|Quigukigir|Rainbow|Red|Relic|Robot|Royal|Royalboy|Royalgirl|Shadow|Silver|Sketch|Skunk|Slushie|Snot|Snow|Speckled|Split|Sponge|Spotted|Starry|Stealthy|Steampunk|Stone|Strawberry|Striped|Swampgas|Thornberry|Tomato|Toy|Transparent|Tyrannian|Ummagine|Usukiboy|Usukigirl|Water|White|Woodland|Wraith|Yellow|Zombie)\s(Acara|Aisha|Blumaroo|Bori|Bruce|Buzz|Chia|Chomby|Cybunny|Draik|Elephante|Eyrie|Flotsam|Gelert|Gnorbu|Grarrl|Grundo|Hissi|Ixi|Jetsam|Jubjub|Kacheek|Kau|Kiko|Koi|Korbat|Kougra|Krawk|Kyrii|Lenny|Lupe|Lutari|Meerca|Moehog|Mynci|Nimmo|Ogrin|Peophin|Poogle|Pteri|Quiggle|Ruki|Scorchio|Shoyru|Skeith|SlushieChia|Techo|Tonu|Tuskaninny|Uni|Usul|Vandagyre|Wocky|Xweetok|Yurble|Zafara).*/i;
+  let neopet = /.*(8-bit|Agueena|Alien|Angel|Apple|Asparagus|Aubergine|Avocado|Baby|Biscuit|Blue|Blueberry|Brown|Burlap|Camouflage|Candy|Carrot|Checkered|Chocolate|Chokato|Christmas|Clay|Cloud|Coconut|Coffee|Corn|Custard|Darigan|Desert|Dimensional|Disco|Durian|Elderlyboy|Elderlygirl|Electric|Eventide|Faerie|Fire|Garlic|Ghost|Glass|Glowing|Gold|Gooseberry|Grape|Green|Grey|Halloween|Ice|Invisible|Island|Jelly|Juppieswirl|Lemon|Lime|Magma|Mallow|Maractite|Maraquan|Marble|Mosaic|Msp|Mutant|Oilpaint|Onion|Orange|Origami|Pastel|Pea|Peach|Pear|Pepper|Pineapple|Pink|Pirate|Plum|Plushie|Polkadot|Purple|Quiguki|Rainbow|Red|Relic|Robot|Royal|Royalboy|Royalgirl|Shadow|Silver|Sketch|Skunk|Slushie|Snot|Snow|Speckled|Split|Sponge|Spotted|Starry|Stealthy|Steampunk|Stone|Strawberry|Striped|Swampgas|Thornberry|Tomato|Toy|Transparent|Tyrannian|Ummagine|Usuki|Valentine|Water|White|Woodland|Wraith|Yellow|Zombie)\s(Acara|Aisha|Blumaroo|Bori|Bruce|Buzz|Chia|Chomby|Cybunny|Draik|Elephante|Eyrie|Flotsam|Gelert|Gnorbu|Grarrl|Grundo|Hissi|Ixi|Jetsam|Jubjub|Kacheek|Kau|Kiko|Koi|Korbat|Kougra|Krawk|Kyrii|Lenny|Lupe|Lutari|Meerca|Moehog|Mynci|Nimmo|Ogrin|Peophin|Poogle|Pteri|Quiggle|Ruki|Scorchio|Shoyru|Skeith|SlushieChia|Techo|Tonu|Tuskaninny|Uni|Usul|Vandagyre|Wocky|Xweetok|Yurble|Zafara).*/i;
 
   if (neopet.exec(message.content)) {
 
     //extract color and pet from message in lowercase
     var color = neopet.exec(message.content)[1].toLowerCase();
     var pet = neopet.exec(message.content)[2].toLowerCase();
+
+    //special case for usuki
+    if (lowerCaseMessageContent.indexOf("usukiboy") >= 0 || lowerCaseMessageContent.indexOf("usuki boy") >= 0  || lowerCaseMessageContent.indexOf("quigukiboy") >= 0  || lowerCaseMessageContent.indexOf("quiguki boy") >= 0  ){
+      if (pet === "quiggle") color = "quigukiboy";
+      else color = "usukiboy";
+    }
+
+    if (lowerCaseMessageContent.indexOf("usukigirl") >= 0 || lowerCaseMessageContent.indexOf("usuki girl") >= 0 || lowerCaseMessageContent.indexOf("quigukigirl") >= 0  || lowerCaseMessageContent.indexOf("quiguki girl") >= 0 ){
+      if (pet === "quiggle") color = "quigukigirl";
+      else color = "usukigirl";
+    }
+
+    //if someone mentions usuki - rng boy or girl
+    var usukis = ["usukiboy", "usukigirl"];
+    //special quiguki case
+    if (pet === "quiggle") usukis = ["quigukiboy", "quigukigirl"];
+    
+    if (color === "usuki" || color === "quiguki") color = usukis[getRandomInt(usukis.length)];
 
     //special case for pineapple since it contains "apple"
     if (lowerCaseMessageContent.indexOf("pineapple") >= 0) {
@@ -137,7 +155,7 @@ client.on("messageCreate", (message) => {//Do Not Close This Function Till Later
     var royals = ["royalboy", "royalgirl"];
     if (color === "royal") color = royals[getRandomInt(royals.length)];
 
-    //Capitalize Pet Names
+    //Capitalize Certain Pet Names
     var petCap = pet.charAt(0).toUpperCase() + pet.slice(1);
     //special case for JubJub
     if (pet === "jubjub") petCap = "JubJub";
@@ -173,6 +191,7 @@ client.on("messageCreate", (message) => {//Do Not Close This Function Till Later
           m.react("🥺");
         });
       }
+
       else {
 
         //import responses need to fix this so the + variables work
